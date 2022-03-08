4decd0205dc0b007a67fc925cde35ebf0dc89dc4
Nizom Sidiq <nizomsidiq@gmail.com>
2022-03-08 07:39:24 +0700

    setup event listeners immediately

--- app/camera/module.py
+++ app/camera/module.py
@@ -6,6 +6,6 @@ from .routes import router
 
 
 def register_module(app: FastAPI):
-    app.add_event_handler("startup", lambda: setup_event_listeners(EventsEmitter.instance()))
+    setup_event_listeners(EventsEmitter.instance())
 
     app.include_router(router)
